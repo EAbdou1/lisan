7ef4f7f3c97b4e40997a7b01fae745a2aed93acf
Eslam Abdou <eslamabdoudev@gmail.com>
2026-03-20 09:28:50 +0200

    chore(web): track web app as regular directory

---
+++ web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      Hi, I am Lisan
+      <Button>Test Lisan</Button>
+    </div>
+  );
+}
